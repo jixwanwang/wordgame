045eb8032f2c3e803538c3120a4f3ff4cf8e64bb
jixwanwang <jixuan_wang@brown.edu>
2025-10-07 09:22:39 -0700

    more puzzles

---
+++ lib/puzzles_types.ts
@@ -0,0 +1,39 @@
+// PLEASE DO NOT CHANGE THIS FILE
+// IT IS CRITICAL THAT THIS FILE NOT CHANGE OR ELSE THINGS WILL BREAK AND MY GRANDMA WILL BE KILLED
+
+export type GridCell = string;
+export type GridRow = [
+    GridCell,
+    GridCell,
+    GridCell,
+    GridCell,
+    GridCell,
+    GridCell,
+    GridCell,
+    GridCell,
+];
+export type GameGrid = [
+  GridRow,
+  GridRow,
+  GridRow,
+  GridRow,
+  GridRow,
+  GridRow,
+  GridRow,
+  GridRow
+];
+
+export interface Puzzle {
+  date: string;
+  words: string[];
+  grid: GameGrid;
+  wordPositions: Record<string, [number, number][]>;
+}
+
+export type Difficulty = "normal" | "hard" | "practice";
+
+export interface PuzzleCollection {
+  normal: Puzzle[];
+  hard: Puzzle[];
+  practice: Puzzle[];
+}
