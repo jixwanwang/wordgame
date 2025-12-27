3cd54184d3c657c05878e908d3e11fefed68555f
jixwanwang <jixuan_wang@brown.edu>
2025-12-26 18:35:21 -0800

    remove react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Game from "@/pages/game";
 
@@ -17,11 +15,9 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Router />
+    </TooltipProvider>
   );
 }
 
