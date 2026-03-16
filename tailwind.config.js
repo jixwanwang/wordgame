7e9a42147eae05b39c35100bcb7be0ed4b07fbd7
jixwanwang <jixuan_wang@brown.edu>
2026-03-15 22:34:34 -0700

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,7 +48,7 @@ export default {
         sans: ["var(--font-sans)"],
         serif: ["var(--font-serif)"],
         mono: ["var(--font-mono)"],
-        title: ["Funnel Display", "var(--font-sans)"],
+        title: ["var(--font-display)"],
       },
     },
   },
